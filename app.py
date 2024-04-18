b1d3b9da3c0de2b0d206c052a6e87314f5e03b9e
rajeshbbalam <rajeshbbalam@gmail.com>
2024-04-18 18:14:07 +0530

    updated

--- app.py
+++ app.py
@@ -9,15 +9,24 @@ import bubble_msg_data as bsd
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="nextaqua-22991")
+tab1, tab2 = st.tabs(["NextFarmBubbleTables", "Farms"])
 
-# checktray_ref = db.collection("checktraydata")
-# checktray_docs = (
-#     db.collection("checktraydata").where("isRealSite", "==", True)
-#     .stream()
-# )
-# # And then render each post, using some light Markdown
-# for doc in checktray_docs:
-# 	fd.displayDocument(doc)
+with tab1:
+    if st.button('Show NextFarmTables'):
+    #/nextfarm_messages/AQU1304248352/cycle1/messages/data
+        msgs_docs = (
+         db.collection("nextfarm_messages/AQU1304248352/cycle1/messages/data").stream()
+        )
+        for doc in msgs_docs:
+            bsd.displayDocument(doc)
+with tab2:          
+    if st.button('Show Farms'):
+       checktray_docs = (
+            db.collection("checktraydata").where("isRealSite", "==", True).stream()
+        )
+       for doc in checktray_docs:
+	       fd.displayDocument(doc)
+    
 
 
 # doc_ref = db.collection("checktraydata").document("AGV2402246188")
@@ -25,11 +34,5 @@ db = firestore.Client(credentials=creds, project="nextaqua-22991")
 # fd.displayDocument(doc)
 # Add final separator to end of grid
 
-#/nextfarm_messages/AQU1304248352/cycle1/messages/data
-msgs_docs = (
-    db.collection("nextfarm_messages/AQU1304248352/cycle1/messages/data").stream()
-)
-for doc in msgs_docs:
-    bsd.displayDocument(doc)
 
 
--- bubble_msg_data.py
+++ bubble_msg_data.py
@@ -6,9 +6,11 @@ def getDictValue(dictObj,strKey):
     return None
 def displayDocument(doc):
     doc_data = doc.to_dict()
+    tblType = getDictValue(doc_data,'tableType')
     st.markdown(f"**Table Type:** {getDictValue(doc_data,'tableType')} **docId:** {doc.id}")
-    df = pd.DataFrame(doc_data['tabledata'])
-    st.dataframe(df)
+    st.table(getDictValue(doc_data,'tabledata'))
+    #df = pd.DataFrame(getDictValue(doc_data,'tabledata'))
+    #st.dataframe(df)
     with st.popover("More Information"):
          st.json(doc_data)
     st.markdown("---")

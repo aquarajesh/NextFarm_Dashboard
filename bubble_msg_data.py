086ded0846986f3bdfa5733e2e90693121343e90
aquarajesh <rajesh@aquaexchange.com>
2024-05-02 18:52:58 +0530

    showing crops_test collection

--- bubble_msg_data.py
+++ bubble_msg_data.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-def getDictValue(dictObj,strKey):
+def getDictValue(dictObj,strKey)->any:
     if strKey in dictObj.keys():
         return dictObj[strKey]
     return None
@@ -8,7 +8,16 @@ def displayDocument(doc):
     doc_data = doc.to_dict()
     tblType = getDictValue(doc_data,'tableType')
     st.markdown(f"**Table Type:** {getDictValue(doc_data,'tableType')} **docId:** {doc.id}")
-    st.table(getDictValue(doc_data,'tabledata'))
+    tabledata = getDictValue(doc_data,'tabledata')
+    if tabledata:
+       if tblType.lower() == 'croptable':
+          newtabledata = tabledata["cropPonds"]
+          for row in newtabledata:
+              row["startDate"] = tabledata["startDate"]
+              row["name"] = tabledata["name"]
+          st.table(newtabledata)
+       else:
+          st.table(tabledata)
     #df = pd.DataFrame(getDictValue(doc_data,'tabledata'))
     #st.dataframe(df)
     with st.popover("More Information"):

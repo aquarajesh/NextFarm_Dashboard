9a81b351ce5496c89dd6c83dc200cb694d6c715a
aquarajesh <rajesh@aquaexchange.com>
2024-05-02 17:27:05 +0530

    analysing crop tables

--- app.py
+++ app.py
@@ -9,21 +9,26 @@ import bubble_msg_data as bsd
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="nextaqua-22991")
-tab1, tab2 = st.tabs(["NextFarmBubbleTables", "Farms"])
-
+tabAnlyse,tab1, tab2 = st.tabs(["Analyse Tables","NextFarmBubbleTables", "Farms"])
+with tabAnlyse:
+     message_path = st.text_input('Enter messages Collection Path:', '/crops_test/BAV1812238444/croptables')
+     st.write('The current messagges from:', message_path)
+     if message_path:
+         msgs_docs = db.collection(message_path).get()
+         for doc in msgs_docs:
+             bsd.displayDocument(doc)
+     
 with tab1:
       message_path = st.text_input('Enter messages Collection Path:', 'nextfarm_messages/AQU1304248352/cycle1/messages/data')
       st.write('The current messagges from:', message_path)
        #/nextfarm_messages/AQU1304248352/cycle1/messages/data
       if message_path:
-         msgs_docs = (db.collection(message_path).stream())
+         msgs_docs = db.collection(message_path).get()
          for doc in msgs_docs:
              bsd.displayDocument(doc)
 with tab2:          
     if st.button('Show Farms'):
-       checktray_docs = (
-            db.collection("checktraydata").where("ax_django_id", ">", 1).stream()
-        )
+       checktray_docs = db.collection("checktraydata").where("ax_django_id", ">", 1).get()
        for doc in checktray_docs:
 	       fd.displayDocument(doc)
 
